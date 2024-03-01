01965a91b3dc02c8c5bb47597a1f4e20240b115f
Daniele Ferneti <108084627+DanieleFerneti@users.noreply.github.com>
2024-03-01 11:25:19 +0100

    Create if_else.rs

---
+++ if_else.rs
@@ -0,0 +1,13 @@
+if 1 == 2 {
+    println!("True, the numbers are equal."); // 
+} else {
+    println!("False, the numbers are not equal.");
+}
+
+let formal = true;
+let greeting = if formal { // if used here as an expression
+    "Good day to you."     // return a String
+} else {
+    "Hey!"                 // return a String
+};
+println!("{}", greeting)   // prints "Good day to you."
